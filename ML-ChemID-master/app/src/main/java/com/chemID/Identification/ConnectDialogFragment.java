85f8d153d9a96fd9e91b653f5d0ab4aef8511126
Rex Liu <rexypad@gmail.com>
2020-11-21 12:33:51 +0800

    11/20 Updated Code for Research Poster

---
+++ ML-ChemID-master/app/src/main/java/com/chemID/Identification/ConnectDialogFragment.java
@@ -0,0 +1,32 @@
+package com.allanbayntun.bayntunglare;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+public class ConnectDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("LOL")
+                .setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+    public void show() {
+        this.show(getFragmentManager(), "Connecctioiot");
+    }
+}
---
+++ ML-ChemID-master/app/src/main/java/com/chemID/Identification/ConnectTask.java
@@ -0,0 +1,57 @@
+package com.allanbayntun.bayntunglare;
+
+import android.os.AsyncTask;
+import android.widget.EditText;
+
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+class ConnectTask extends AsyncTask<String, String,String> {
+
+    DataOutputStream out;
+    int sequence = 0;
+    String ip = "";
+    public ConnectTask(DataOutputStream o, String i) {
+        super();
+        out = o;
+        ip = i;
+    }
+    protected String doInBackground(String... urls) {
+        try {
+            Socket client = new Socket(ip, 1735);
+            OutputStream outToServer = client.getOutputStream();
+            out = new DataOutputStream(outToServer);
+            byte[] hello = {0x01, 0x03, 0x00, 0x00, 0x00, 0x00};
+            out.write(hello);
+            out.write(0x05);
+            double[] val ={1.0, 2.0, 3.0};
+            byte[] g = {0x11, 0x00, 0x00, 0x00, (byte) sequence, 0x11, (byte) val.length};
+            byte[] f = new byte[7 + (8 * val.length)];
+            for(int i = 0; i < g.length; i++) {
+                f[i] = g[i];
+            }
+            for (int i = 0; i < val.length; i++) {
+                byte[] k = ByteBuffer.allocate(8).putDouble(val[i]).array();
+                for (int j = 0; j < k.length; j++) {
+                    f[((i+1) * 8) + j -1] = k[j];
+                }
+            }
+            try {
+                System.out.println("written");
+                out.write(f);
+                for (int i = 0; i< f.length; i++) {
+                    System.out.print(f[i] + ", ");
+                }
+                System.out.println();
+                ++sequence;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return "connected";
+        } catch (Exception e) {
+            return "failed";
+        }
+    };
+}
