cb5bfce6f4ddba98288e1661b987e4dc4439d6d3
Rex Liu <rexypad@gmail.com>
2022-09-14 08:29:29 +0800

    Removed build files

---
+++ ChemID-master/app/src/main/java/com/chemid/identification/MainActivity.java
@@ -0,0 +1,374 @@
+package com.chemid.identification;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.media.ImageReader.OnImageAvailableListener;
+import android.os.SystemClock;
+import android.text.InputType;
+import android.util.Size;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+import com.chemid.identification.OverlayView.DrawCallback;
+import com.chemid.identification.env.BorderedText;
+import com.chemid.identification.env.ImageUtils;
+import com.chemid.identification.tracking.MultiBoxTracker;
+
+public class MainActivity extends CameraActivity implements OnImageAvailableListener {
+
+    // Configuration values for the prepackaged SSD model.
+    private static final int TF_OD_API_INPUT_SIZE = 300;
+    private static final boolean TF_OD_API_IS_QUANTIZED = true;
+    private static final String TF_OD_API_MODEL_FILE = "detect.tflite";
+    private static final String TF_OD_API_LABELS_FILE = "labels.txt";
+
+    // Which detection model to use: by default uses Tensorflow Object Detection API frozen
+    // checkpoints.
+    private enum DetectorMode {
+        TF_OD_API;
+    }
+
+    private static final DetectorMode MODE = DetectorMode.TF_OD_API;
+
+    // Minimum detection confidence to track a detection.
+    private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.6f;
+
+    private static final boolean MAINTAIN_ASPECT = false;
+
+    private static final Size DESIRED_PREVIEW_SIZE = new Size(640, 480);
+
+    private static final boolean SAVE_PREVIEW_BITMAP = false;
+    private static final float TEXT_SIZE_DIP = 10;
+
+    private Integer sensorOrientation;
+
+    private Classifier detector;
+
+    private long lastProcessingTimeMs;
+    private Bitmap rgbFrameBitmap = null;
+    private Bitmap croppedBitmap = null;
+    private Bitmap cropCopyBitmap = null;
+
+    private boolean computingDetection = false;
+
+    private long timestamp = 0;
+
+    private Matrix frameToCropTransform;
+    private Matrix cropToFrameTransform;
+
+    private MultiBoxTracker tracker;
+
+    private byte[] luminanceCopy;
+
+    private BorderedText borderedText;
+
+    private DataOutputStream out = null;
+
+    private boolean connected_net = false, stream = false;
+
+    private int sequence = 100000000;
+
+    public MainActivity() {
+        super();
+    }
+
+    public void connect(View view) {
+        final MainActivity t = this;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Connect");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_URI);
+        builder.setView(input);
+        builder.setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+                try {
+                    ConnectTask c = new ConnectTask(out, /*((EditText)findViewById(R.id.ip_txt)).getText().toString()*/"dick");
+                    String s = c.execute().get();
+                    if (s.equals("connected")) {
+                        connected_net = true;
+                        AlertDialog.Builder b1 = new AlertDialog.Builder(t);
+                        b1.setMessage("Connection Succeeded");
+                        b1.show();
+                    } else {
+                        AlertDialog.Builder b2 = new AlertDialog.Builder(t);
+                        b2.setTitle("Failed");
+                        b2.setMessage("Connection Failed");
+                        b2.show();
+                    }
+                } catch(Exception e) {
+                    AlertDialog.Builder b2 = new AlertDialog.Builder(t);
+                    b2.setTitle("Failed");
+                    b2.show();
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        builder.setCancelable(false);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void play(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        if (!stream && connected_net) {
+            stream = true;
+            builder.setTitle("Streaming");
+            builder.setMessage("Streaming to Network Tables Now");
+            builder.show();
+            return;
+        }
+        stream = false;
+        builder.setTitle("Not Streaming");
+        builder.setMessage("Unable to stream to network tables");
+        builder.show();
+    }
+
+    @Override
+    public void onPreviewSizeChosen(final Size size, final int rotation) {
+        final float textSizePx =
+                TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP, TEXT_SIZE_DIP, getResources().getDisplayMetrics());
+        borderedText = new BorderedText(textSizePx);
+        borderedText.setTypeface(Typeface.MONOSPACE);
+
+        tracker = new MultiBoxTracker(this);
+
+        int cropSize = TF_OD_API_INPUT_SIZE;
+
+        try {
+            detector =
+                    TFLiteObjectDetectionAPIModel.create(
+                            getAssets(),
+                            TF_OD_API_MODEL_FILE,
+                            TF_OD_API_LABELS_FILE,
+                            TF_OD_API_INPUT_SIZE,
+                            TF_OD_API_IS_QUANTIZED);
+            cropSize = TF_OD_API_INPUT_SIZE;
+        } catch (final IOException e) {
+            Toast toast =
+                    Toast.makeText(
+                            getApplicationContext(), "Classifier could not be initialized", Toast.LENGTH_SHORT);
+            toast.show();
+            finish();
+        }
+
+
+        previewWidth = size.getWidth();
+        previewHeight = size.getHeight();
+
+        sensorOrientation = rotation - getScreenOrientation();
+
+        rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Config.ARGB_8888);
+        croppedBitmap = Bitmap.createBitmap(cropSize, cropSize, Config.ARGB_8888);
+
+        frameToCropTransform =
+                ImageUtils.getTransformationMatrix(
+                        previewWidth, previewHeight,
+                        cropSize, cropSize,
+                        sensorOrientation, MAINTAIN_ASPECT);
+
+        cropToFrameTransform = new Matrix();
+        frameToCropTransform.invert(cropToFrameTransform);
+
+        trackingOverlay = (OverlayView) findViewById(R.id.tracking_overlay);
+        trackingOverlay.addCallback(
+                new DrawCallback() {
+                    @Override
+                    public void drawCallback(final Canvas canvas) {
+                        tracker.draw(canvas);
+                        if (isDebug()) {
+                            tracker.drawDebug(canvas);
+                        }
+                    }
+                });
+
+        addCallback(
+                new DrawCallback() {
+                    @Override
+                    public void drawCallback(final Canvas canvas) {
+                        if (!isDebug()) {
+                            return;
+                        }
+                        final Bitmap copy = cropCopyBitmap;
+                        if (copy == null) {
+                            return;
+                        }
+
+                        final int backgroundColor = Color.argb(100, 0, 0, 0);
+                        canvas.drawColor(backgroundColor);
+
+                        final Matrix matrix = new Matrix();
+                        final float scaleFactor = 2;
+                        matrix.postScale(scaleFactor, scaleFactor);
+                        matrix.postTranslate(
+                                canvas.getWidth() - copy.getWidth() * scaleFactor,
+                                canvas.getHeight() - copy.getHeight() * scaleFactor);
+                        canvas.drawBitmap(copy, matrix, new Paint());
+
+                        final Vector<String> lines = new Vector<String>();
+                        if (detector != null) {
+                            final String statString = detector.getStatString();
+                            final String[] statLines = statString.split("\n");
+                            for (final String line : statLines) {
+                                lines.add(line);
+                            }
+                        }
+                        lines.add("");
+
+                        lines.add("Frame: " + previewWidth + "x" + previewHeight);
+                        lines.add("Crop: " + copy.getWidth() + "x" + copy.getHeight());
+                        lines.add("View: " + canvas.getWidth() + "x" + canvas.getHeight());
+                        lines.add("Rotation: " + sensorOrientation);
+                        lines.add("Inference time: " + lastProcessingTimeMs + "ms");
+
+                        borderedText.drawLines(canvas, 10, canvas.getHeight() - 10, lines);
+                    }
+                });
+    }
+
+    OverlayView trackingOverlay;
+
+    @Override
+    protected void processImage() {
+        ++timestamp;
+        final long currTimestamp = timestamp;
+        byte[] originalLuminance = getLuminance();
+        tracker.onFrame(
+                previewWidth,
+                previewHeight,
+                getLuminanceStride(),
+                sensorOrientation,
+                originalLuminance,
+                timestamp);
+        trackingOverlay.postInvalidate();
+
+        // No mutex needed as this method is not reentrant.
+        if (computingDetection) {
+            readyForNextImage();
+            return;
+        }
+        computingDetection = true;
+
+        rgbFrameBitmap.setPixels(getRgbBytes(), 0, previewWidth, 0, 0, previewWidth, previewHeight);
+
+        if (luminanceCopy == null) {
+            luminanceCopy = new byte[originalLuminance.length];
+        }
+        System.arraycopy(originalLuminance, 0, luminanceCopy, 0, originalLuminance.length);
+        readyForNextImage();
+
+        final Canvas canvas = new Canvas(croppedBitmap);
+        canvas.drawBitmap(rgbFrameBitmap, frameToCropTransform, null);
+        // For examining the actual TF input.
+        if (SAVE_PREVIEW_BITMAP) {
+            ImageUtils.saveBitmap(croppedBitmap);
+        }
+
+        runInBackground(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        final long startTime = SystemClock.uptimeMillis();
+                        final List<Classifier.Recognition> results = detector.recognizeImage(croppedBitmap);
+                        lastProcessingTimeMs = SystemClock.uptimeMillis() - startTime;
+
+                        cropCopyBitmap = Bitmap.createBitmap(croppedBitmap);
+                        final Canvas canvas = new Canvas(cropCopyBitmap);
+                        final Paint paint = new Paint();
+                        paint.setColor(Color.RED);
+                        paint.setStyle(Style.STROKE);
+                        paint.setStrokeWidth(2.0f);
+
+                        float minimumConfidence = MINIMUM_CONFIDENCE_TF_OD_API;
+                        switch (MODE) {
+                            case TF_OD_API:
+                                minimumConfidence = MINIMUM_CONFIDENCE_TF_OD_API;
+                                break;
+                        }
+
+                        final List<Classifier.Recognition> mappedRecognitions =
+                                new LinkedList<Classifier.Recognition>();
+
+                        for (final Classifier.Recognition result : results) {
+                            final RectF location = result.getLocation();
+                            if (location != null && result.getConfidence() >= minimumConfidence) {
+                                canvas.drawRect(location, paint);
+                                cropToFrameTransform.mapRect(location);
+                                result.setLocation(location);
+                                mappedRecognitions.add(result);
+                            }
+                        }
+
+                        tracker.trackResults(mappedRecognitions, luminanceCopy, currTimestamp);
+                        System.out.println("Recs: " + mappedRecognitions);
+                        if (stream && connected_net && mappedRecognitions.size() > 0) {
+                            System.out.println("lol");
+                            RectF temp  = mappedRecognitions.get(0).getLocation();
+                            float x = ((LinkedList<Classifier.Recognition>) mappedRecognitions).getFirst().getConfidence();
+                            double[] val ={temp.width(), temp.height(), temp.centerX(), temp.centerY(), (double) x};
+                            byte[] g = {0x11, 0x00, 0x00, 0x00, (byte) sequence, 0x11, (byte) val.length};
+                            byte[] f = new byte[7 + (8 * val.length)];
+                            for(int i = 0; i < g.length; i++) {
+                                f[i] = g[i];
+                            }
+                            for (int i = 0; i < val.length; i++) {
+                                byte[] k = ByteBuffer.allocate(8).putDouble(val[i]).array();;
+                                for (int j = 0; j < k.length; j++) {
+                                    f[((i+1) * 8) + j -1] = k[j];
+                                }
+                            }
+                            try {
+                                System.out.println("written");
+                                out.write(f);
+                                for (int i = 0; i< f.length; i++) {
+                                    System.out.print(f[i] + ", ");
+                                }
+                                System.out.println();
+                                ++sequence;
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        trackingOverlay.postInvalidate();
+
+                        requestRender();
+                        computingDetection = false;
+                    }
+                });
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected Size getDesiredPreviewFrameSize() {
+        return DESIRED_PREVIEW_SIZE;
+    }
+
+    @Override
+    public void onSetDebug(final boolean debug) {
+    }
+}
---
+++ ChemID-master/app/src/main/java/com/chemid/identification/env/AssetUtils.java
@@ -0,0 +1,63 @@
+package com.chemid.identification.env;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/** Utilities for dealing with assets. */
+public class AssetUtils {
+
+    private static final String TAG = AssetUtils.class.getSimpleName();
+
+    private static final int BYTE_BUF_SIZE = 2048;
+
+    /**
+     * Copies a file from assets.
+     *
+     * @param context application context used to discover assets.
+     * @param assetName the relative file name within assets.
+     * @param targetName the target file name, always over write the existing file.
+     * @throws IOException if operation fails.
+     */
+    public static void copy(Context context, String assetName, String targetName) throws IOException {
+
+        Log.d(TAG, "creating file " + targetName + " from " + assetName);
+
+        File targetFile = null;
+        InputStream inputStream = null;
+        FileOutputStream outputStream = null;
+
+        try {
+            AssetManager assets = context.getAssets();
+            targetFile = new File(targetName);
+            inputStream = assets.open(assetName);
+            // TODO(kanlig): refactor log messages to make them more useful.
+            Log.d(TAG, "Creating outputstream");
+            outputStream = new FileOutputStream(targetFile, false /* append */);
+            copy(inputStream, outputStream);
+        } finally {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+    }
+
+    private static void copy(InputStream from, OutputStream to) throws IOException {
+        byte[] buf = new byte[BYTE_BUF_SIZE];
+        while (true) {
+            int r = from.read(buf);
+            if (r == -1) {
+                break;
+            }
+            to.write(buf, 0, r);
+        }
+    }
+}
